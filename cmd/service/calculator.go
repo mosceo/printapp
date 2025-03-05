d2be6e971dca6b416e1e18fd36a71401a94f0ac9
Roman <kunin.ra@dns-shop.ru>
2025-03-05 21:59:21 +1000

    Autoscale processes

---
+++ cmd/service/calculator.go
@@ -0,0 +1,36 @@
+package main
+
+import "time"
+
+// Calculator скажет как быстро расло число (в сек)
+// по сравнению с предыдущим измерением.
+type Calculator struct {
+	t time.Time
+	n int
+
+	prevRate float64
+}
+
+func NewCalculator() *Calculator {
+	return &Calculator{
+		t: time.Now(),
+	}
+}
+
+func (c *Calculator) Set(n int) (float64, bool) {
+	dt := time.Since(c.t)
+	dn := n - c.n
+
+	rate := float64(dn) / dt.Seconds()
+
+	c.t = time.Now()
+	c.n = n
+
+	var grown bool
+	if rate > c.prevRate {
+		grown = true
+	}
+	c.prevRate = rate
+
+	return rate, grown
+}
--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
+
+// fillRedis наполнит Редис данными (n ключей).
+func fillRedis(client *redis.Client, n int) error {
+	for i := 0; i < n; i++ {
+		key := fmt.Sprintf("%d", i)
+		val := fmt.Sprintf("%d", i)
+
+		err := client.Set(context.Background(), key, val, 0).Err()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 // stressTestRedis отправит n кол-во set запросов в Редис.
 func stressTestRedis(client *redis.Client, n int) error {
@@ -22,7 +38,55 @@ func stressTestRedis(client *redis.Client, n int) error {
 	return nil
 }
 
+type Stresser struct {
+	client *redis.Client
+
+	counter  int64 // Кол-во выполненных запросов
+	channels []chan struct{}
+}
+
+func NewStresser(client *redis.Client) *Stresser {
+	return &Stresser{
+		client: client,
+	}
+}
+
+func (s *Stresser) AddProccess() {
+	done := make(chan struct{})
+	s.channels = append(s.channels, done)
+	go func() {
+		for {
+			err := s.client.Get(context.Background(), "key").Err()
+			if err != nil {
+				log.Printf("process died: %s", err)
+				return
+			}
+
+			atomic.AddInt64(&s.counter, 1)
+
+			select {
+			case <-done:
+				return
+			default:
+			}
+		}
+	}()
+}
+
+func (s *Stresser) SubProccess() {
+	done := s.channels[len(s.channels)-1]
+	close(done)
+	s.channels = s.channels[0 : len(s.channels)-1]
+}
+
+func (s *Stresser) GetCount() int {
+	c := atomic.LoadInt64(&s.counter)
+	return int(c)
+}
+
 func main() {
+	// Создадим подключение к Редис
+	//
 	client := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "",
@@ -31,23 +95,27 @@ func main() {
 
 	err := client.Ping(context.Background()).Err()
 	if err != nil {
-		log.Println("[ERR] redis: pong not received")
+		log.Printf("[ERR] redis: ping: %s", err)
 	} else {
 		log.Println("redis: pong received")
 	}
 
+	// Наполним Редис первоначальными данными
+	err = fillRedis(client, 1000)
+	if err != nil {
+		log.Printf("[ERR] redis: наполнение данными: %s", err)
+	} else {
+		log.Println("redis: pong received")
+	}
+
+	s := NewStresser(client)
+	s.AddProccess()
+
 	go func() {
 		for {
-			t1 := time.Now()
-			err := stressTestRedis(client, 1000)
-			dur := time.Since(t1)
+			s.GetCount()
 
-			if err != nil {
-				log.Println("[ERR]", err)
-			} else {
-				log.Printf("redis stress test: %s", dur.Round(time.Millisecond))
-			}
-			time.Sleep(10 * time.Second)
+			time.Sleep(5 * time.Second)
 		}
 	}()
 
